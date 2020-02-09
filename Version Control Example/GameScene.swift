d8fcc73d8a3ca67b80a9c066ca32a1517fe0a098
Vivek Rai <vivekraiiosdev@gmail.com>
2020-02-09 21:40:38 +0530

    Added Palayer1

--- Version Control Example/GameScene.swift
+++ Version Control Example/GameScene.swift
@@ -11,10 +11,13 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-   
-    
     override func didMove(to view: SKView) {
-   
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        let player1 = SKSpriteNode(imageNamed: "player1")
+        player1.setScale(0.5)
+        player1.position = CGPoint.zero
+        player1.zPosition = 1.0
+        addChild(player1)
     }
     
 
