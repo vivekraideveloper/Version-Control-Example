11ce5380d78f323068624a24fa99db7604b0b458
Vivek Rai <vivekraiiosdev@gmail.com>
2020-02-09 21:49:02 +0530

    added crazy clown

--- Version Control Example/GameScene.swift
+++ Version Control Example/GameScene.swift
@@ -12,12 +12,20 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
         let player1 = SKSpriteNode(imageNamed: "player1")
         player1.setScale(0.5)
-        player1.position = CGPoint.zero
+        player1.position = CGPoint(x: 0, y: -100)
         player1.zPosition = 1.0
         addChild(player1)
+        
+        let player2 = SKSpriteNode(imageNamed: "player2")
+        player2.setScale(0.5)
+        player2.position = CGPoint(x: 0, y: 100)
+        player2.zPosition = 1.0
+        addChild(player2)
     }
     
 
